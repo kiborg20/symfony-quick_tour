b849af2412730f29e5e87560e479a3cd792ad712
kiborg20 <vladimir.stenderchuk@gamil.com>
2021-04-03 22:25:16 +0500

    final Twig first view

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,8 +3,10 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class DefaultController
+// class DefaultController
+class DefaultController extends AbstractController
 {
     // public function index()
     /**
@@ -13,7 +15,10 @@ class DefaultController
     public function index($name)
     {
         // return new Response('Hello!');
-        return new Response("Привет, $name!");
+        // return new Response("Привет, $name!");
+        return $this->render('default/index.html.twig', [
+            'name' => $name,
+        ]);
     }
 
     /**
